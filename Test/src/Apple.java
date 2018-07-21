53fb25a4b8af67618bf9df26de41aab85dc4cdd0
Kevin Durant <2637206895@qq.com>
2018-07-21 21:06:48 +0800

    Add setSize() method.

--- Test/src/Apple.java
+++ Test/src/Apple.java
@@ -1,11 +1,16 @@
 
 public class Apple {
-	public static void main(String[]args) {
+	public static void main(String[] args) {
 		System.out.println("OK.");
 		System.out.println("This is an Apple");
 	}
-	
+
 	public void sayName() {
 		System.out.println("I am Apple.");
 	}
+
+	public void setSize() {
+		int size = 100;
+	}
+
 }
